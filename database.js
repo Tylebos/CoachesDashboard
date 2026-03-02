de5652393aa4913b303976dcf6fc30282079a42c
Tylebos <tybo6801@colorado.edu>
2026-03-02 14:38:56 -0600

    Initial connection to DB est

--- database.js
+++ database.js
@@ -1,1 +1,18 @@
-import mysql from "mysql2"
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise() // Allows for async functions
+
+async function getTeams() {
+    const [rows] = await pool.query("SELECT * FROM Teams")
+    return rows
+}
+
+const teams = await getTeams()
+console.log(teams)
